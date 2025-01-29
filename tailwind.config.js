6ef8bc0f771813deadd4b43864a50442d34ffe59
SAMALT68 <141881625+SAMALT68@users.noreply.github.com>
2025-01-29 17:48:00 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+document.documentElement.classList.toggle(
+  "dark",
+  localStorage.currentTheme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
+);
+// Whenever the user explicitly chooses light mode
+localStorage.currentTheme = "light";
+// Whenever the user explicitly chooses dark mode
+localStorage.currentTheme = "dark";
+// Whenever the user explicitly chooses to respect the OS preference
+localStorage.removeItem("theme");
